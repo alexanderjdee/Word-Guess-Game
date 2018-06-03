30532afe795316a9da6cf248f7b12ee0133d20d8
adee <adee4990@gmail.com>
2018-06-03 18:18:35 -0400

    Assignment requirements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 // GLOBAL VARIABLES
-var words = ["captain", "cannon", "cutlass", "galleon", "maroon", 
-"mutiny", "parrot", "pistol", "rum", "scurvy", "treasure"]
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
+"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var words = ["BOOTY", "CAPTAIN", "CANNON", "CUTLASS", "GALLEON", "HOOK", "KEELHAUL", 
+"MAROON", "MAST", "MUTINY", "PARROT", "PISTOL", "PLANK", "PLUNDER", "RUM", "SCURVY", "TREASURE"]
 
 var winsText = document.getElementById("wins");
 var currentWordText = document.getElementById("currentWord");
@@ -9,79 +12,120 @@ var lettersGuessedText = document.getElementById("lettersGuessed");
 
 //GLOBAL FUNCTIONS
 
-
-//OBJECTS
+//GAME OBJECT
 var game = {
     wins: 0,
     word: "",
     wordArray: [],
     currentWord: [],
-    currentWordIsSet: false,
     guessesRemaining: 0,
-    currentLetter: "",
     lettersGuessed: [],
+    guessWasCorrect: false,
+    gameOver: false,
 
     initializeGame: function(){
         this.guessesRemaining = 12;
-        this.currentWord = [];
+        guessesRemainingText.textContent = this.guessesRemaining;
+        this.lettersGuessed = [];
+        lettersGuessedText.textContent = this.lettersGuessed;
         this.chooseWord();
         this.setCurrentWord();
     },
     
     chooseWord: function(){
         this.word = words[Math.floor(Math.random() * words.length)];
-        console.log(this.word);
     },
 
     setCurrentWord: function(){
         this.wordArray = this.word.split("");
-        console.log(this.wordArray);
+        this.currentWord = [];
 
         for(i=0; i < this.wordArray.length; i++){
-            if(i == this.wordArray.length-1){
-                this.currentWord[i] = "_";
-            }
-            else{
-                this.currentWord[i] = "_ ";
-            } 
+            this.currentWord[i] = "_";
         }
 
-        this.currentWord = this.currentWord.join("");
-        console.log(this.currentWord);
-        currentWordText.textContent = this.currentWord;
+        this.updateCurrentWordText();
+    },
+
+    updateCurrentWordText: function(index){
+        currentWordText.textContent = this.currentWord.join(" ");
     },
 
     letterCheck: function(input){
-
+        var letter = input;
+        for(i = 0; i <= this.wordArray.length; i++){
+            if(letter === this.wordArray[i]){
+                this.currentWord[i] = this.wordArray[i];
+                this.updateCurrentWordText();
+                this.guessWasCorrect = true;             
+            }
+        }
+        
+        this.updateGuesses(letter);
     },
 
-    updateCurrentWord: function(index){
-
+    updateGuesses: function(guess){
+        if(this.guessWasCorrect === false){
+            for(i = 0; i <= this.lettersGuessed.length + 1; i++)
+            {
+                if(guess === this.lettersGuessed[i]){
+                    break;
+                }
+                else if(i === this.lettersGuessed.length + 1){
+                    this.lettersGuessed.push(guess);
+                    this.guessesRemaining -= 1;
+                    guessesRemainingText.textContent = this.guessesRemaining;
+                    lettersGuessedText.textContent= this.lettersGuessed.join(" ");
+                    break;
+                }
+            }
+            
+            this.loseCheck();
+        }
+        else{
+            this.guessWasCorrect = false;
+            this.winCheck();
+        }
     },
 
-    updateGuesses: function(){
-        this.guessesRemaining -= 1;
-        guessesRemainingText.textContent = this.guessesRemaining;
+    winCheck: function(){
+        for(i = 0; i <= this.wordArray.length; i++)
+        {
+            if(this.currentWord[i] !== this.wordArray[i]){
+                break;
+            }
+            else if(i === this.wordArray.length){
+                this.wins += 1;
+                winsText.textContent = this.wins;
+                alert("X marks the spot. You won!");
+                this.initializeGame(); 
+            } 
+        }
     },
 
-    win: function(){
-        this.wins += 1;
-        winsText.textContent = this.wins;
-        this.initializeGame();
-    },
-
-
+    loseCheck: function(){
+        if(this.guessesRemaining === 0){
+            alert("You walked the plank. Try again!");
+            this.initializeGame();
+        }  
+    }
 }
 
-//LOGIC
+//INITIALIZE GAME AND READ USER INPUT
 
-game.chooseWord();
-game.setCurrentWord();
+game.initializeGame();
 
 document.onkeyup = function(event){
-    var userGuess = event.key;
+    var userInput = event.key;
+    userInput = userInput.toUpperCase();
 
-    game.letterCheck(letterCheck);
+    for(i = 0; i < alphabet.length; i++)
+    {
+        if(userInput === alphabet[i]){
+            game.letterCheck(userInput);
+            break;
+        }
+    }
 }
 
 
